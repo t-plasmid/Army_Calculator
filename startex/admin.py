0e2664e16cbc7d55d7464739715150e20db823d5
Tej Kumar Rai <t_plasmid@yahoo.com>
2022-03-25 19:26:02 +0000

    Bug fixes and feature updates.

--- startex/admin.py
+++ startex/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.StartEx_Plan)
+admin.site.register(models.SX_Vehicle_Data)
+admin.site.register(models.SX_Unit_Detail)
+admin.site.register(models.SX_Vehicle_Detail)
